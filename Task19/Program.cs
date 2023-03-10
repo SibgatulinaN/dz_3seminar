c1dab68ff859887701b8087cbc0e77ca2d463c89
Наталья Сибгатулина <mehryak@mail.ru>
2023-03-10 22:35:12 +0500

    add Task19, Task21, Task23

---
+++ Task19/Program.cs
@@ -0,0 +1,33 @@
+﻿/* Задача 19
+Напишите программу, которая принимает на вход пятизначное число и проверяет, является ли оно палиндромом.
+14212 -> нет
+12821 -> да
+23432 -> да */
+Console.WriteLine("Введите пятизначное число:");
+int number=Convert.ToInt32(Console.ReadLine());
+CheckPalindromNumber(number);
+
+void CheckPalindromNumber(int number)
+{
+    if (number > 9999 & number < 100000)
+    {
+        int division1 = number / 10000;
+        int remainder1 = number % 10;
+
+        if (division1 == remainder1)
+        {
+            number = number / 10;
+            int division2 = (number / 100) % 10;
+            int remainder2 = number % 10;
+            if (division2 == remainder2)
+                Console.WriteLine("Да");
+        }
+        else
+            Console.WriteLine("Нет");
+
+    }
+    else
+        Console.WriteLine("Некорректное число!");
+}
+
+
---
+++ Task21/Program.cs
@@ -0,0 +1,32 @@
+﻿/* Задача 21
+Напишите программу, которая принимает на вход координаты двух точек и находит расстояние между ними в 3D пространстве.
+A (3,6,8); B (2,1,-7), -> 15.84
+A (7,-5, 0); B (1,-1,9) -> 11.53 */
+
+Console.WriteLine("Введите координаты точки A");
+Console.Write("xA :");
+int xA=Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите координаты точки");
+Console.Write("yA :");
+int yA=Convert.ToInt32(Console.ReadLine());
+Console.Write("zA :");
+int zA=Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Введите координаты точки B");
+Console.Write("xB :");
+int xB=Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите координаты точки");
+Console.Write("yB :");
+int yB=Convert.ToInt32(Console.ReadLine());
+Console.Write("zB :");
+int zB=Convert.ToInt32(Console.ReadLine());
+
+Double Distance (int x1, int y1, int z1, int x2, int y2, int z2)
+{
+return  Math.Sqrt((x2-x1)*(x2-x1)+(y2-y1)*(y2-y1)+(z2-z1)*(z2-z1));
+}
+
+double Leng = Distance(xA, yA, zA, xB, yB, zB);
+
+Console.WriteLine($"Расстояние между точкой А и точкой В  равно {Math.Round(Leng, 2,MidpointRounding.ToZero)}");
+
